69618d47b29d285bf3d298159a9d0590ff1e3910
DavidDinkevich <david.dinkevich@gmail.com>
2017-08-09 18:18:37 -0400

    Made TempChanger class work as both an ac and heater

--- SimulatedEcosystem/src/main/TempChanger.java
+++ SimulatedEcosystem/src/main/TempChanger.java
@@ -4,9 +4,18 @@ public class TempChanger {
 	private float minPower, maxPower;
 	private float power;
 	
-	private TempChanger(float minPower, float maxPower) {
+	public TempChanger(float minPower, float maxPower) {
 		this.minPower = minPower;
 		this.maxPower = maxPower;
+	}
+	
+	public TempChanger(TempChanger other) {
+		this(other.minPower, other.maxPower);
+		power = other.power;
+	}
+	
+	public TempChanger() {
+		this(-2f, 2f);
 	}
 	
 	public void setPower(float power) {
@@ -17,11 +26,19 @@ public class TempChanger {
 		return power;
 	}
 	
-	public static TempChanger newAirConditioner() {
-		return new TempChanger(-2, 0);
+	public float getMinPower() {
+		return minPower;
 	}
 	
-	public static TempChanger newHeater() {
-		return new TempChanger(0, 2);
+	public void setMinPower(float minPower) {
+		this.minPower = minPower;
+	}
+	
+	public float getMaxPower() {
+		return maxPower;
+	}
+	
+	public void setMaxPower(float maxPower) {
+		this.maxPower = maxPower;
 	}
 }
