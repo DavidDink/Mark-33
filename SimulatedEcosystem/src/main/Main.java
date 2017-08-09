15c015eb318283c506a2873c49c950e550f1502c
DavidDinkevich <david.dinkevich@gmail.com>
2017-08-09 18:17:21 -0400

    Minor changes

--- SimulatedEcosystem/src/main/Main.java
+++ SimulatedEcosystem/src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 	public static void main(String[] args) {
 		Session sess = new Session();
-		sess.run();
-		sess.saveResultsToFile("data.csv");
+		sess.run(150);
+		Utils.saveResultsToFile(sess);
 	}
 }
