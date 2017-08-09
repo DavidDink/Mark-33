6d75f393ebd00d480513230eaaccdb9c3e8b0ba7
DavidDinkevich <david.dinkevich@gmail.com>
2017-08-09 18:17:13 -0400

    Added more actions, and names for each action

--- SimulatedEcosystem/src/main/Action.java
+++ SimulatedEcosystem/src/main/Action.java
@@ -2,7 +2,21 @@ package main;
 
 public class Action {
 	public static enum Types {
-		AC_ON, AC_OFF, HEATER_ON, HEATER_OFF, NONE;
+		AC_ON("ac on"), AC_OFF("ac off"), HEATER_ON("heater on"),
+		HEATER_OFF("heater off"), AC_ON_HEATER_ON("ac on - heater on"),
+		AC_ON_HEATER_OFF("ac on - heater off"), AC_OFF_HEATER_ON("ac off - heater on"),
+		AC_OFF_HEATER_OFF("ac off - heater off"), NONE("no action");
+		
+		private String name;
+		
+		private Types(String name) {
+			this.name = name;
+		}
+		
+		@Override
+		public String toString() {
+			return name;
+		}
 	}
 	
 	private Types type;
@@ -17,8 +31,13 @@ public class Action {
 		this(Types.NONE, 0);
 	}
 	
+	@Override
+	public String toString() {
+		return type.toString();
+	}
+	
 	public float getCost() {
-		return Math.abs(powerUsed);
+		return powerUsed;
 	}
 
 	public Types getType() {
