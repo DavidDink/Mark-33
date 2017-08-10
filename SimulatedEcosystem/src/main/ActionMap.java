c847ab2f383c839c65215c8f93eb044bae8227a5
DavidDinkevich <david.dinkevich@gmail.com>
2017-08-09 22:42:09 -0400

    Added the concept of a 'State'

--- SimulatedEcosystem/src/main/ActionMap.java
+++ SimulatedEcosystem/src/main/ActionMap.java
@@ -1,6 +1,6 @@
 package main;
 
-public class Action {
+public class ActionMap {
 	public static enum Types {
 		AC_ON("ac on"), AC_OFF("ac off"), HEATER_ON("heater on"),
 		HEATER_OFF("heater off"), AC_ON_HEATER_ON("ac on - heater on"),
@@ -19,29 +19,30 @@ public class Action {
 		}
 	}
 	
-	private Types type;
+	private Types action;
+	private Types state;
 	private float powerUsed;
 	
-	public Action(Types type, float powerUsed) {
-		this.type = type;
+	public ActionMap(Types action, Types state, float powerUsed) {
+		this.action = action;
+		this.state = state;
 		this.powerUsed = powerUsed;
 	}
 	
-	public Action() {
-		this(Types.NONE, 0);
-	}
-	
-	@Override
-	public String toString() {
-		return type.toString();
+	public ActionMap() {
+		this(Types.NONE, Types.NONE, 0);
 	}
 	
 	public float getCost() {
 		return powerUsed;
 	}
 
-	public Types getType() {
-		return type;
+	public Types getAction() {
+		return action;
+	}
+
+	public Types getState() {
+		return state;
 	}
 
 	public float getPowerUsed() {
