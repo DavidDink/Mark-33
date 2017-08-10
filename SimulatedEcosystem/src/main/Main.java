5b591d81528895df993171b54cb7d7b9098c17f4
DavidDinkevich <david.dinkevich@gmail.com>
2017-08-09 20:48:38 -0400

    Minor refactoring

--- SimulatedEcosystem/src/main/Main.java
+++ SimulatedEcosystem/src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 	public static void main(String[] args) {
 		Session sess = new Session();
-		sess.run(150);
-		Utils.saveResultsToFile(sess);
+		sess.run(100);
+		Utils.saveToFile(sess);
 	}
 }
